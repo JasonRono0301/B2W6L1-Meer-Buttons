d642a0f55da146be3bd352ed3168b072ae42c987
Jason R <99057964@mydavinci.nl>
2021-01-04 17:30:21 +0100

    Bezig met de buttons.

--- opdracht B /buttons.js
+++ opdracht B /buttons.js
@@ -1,9 +1,9 @@
 //Lab --> Opdracht B --> 04-01-2021.
 var container = document.getElementById('container');
-for(i=0; i < 31; i++){
+i = 0;
+for(i=1; i < 31; i++){
     var create = document.createElement('button');
     create.innerHTML = i;
-    create.id = i;
     container.appendChild(create);
 }
 
