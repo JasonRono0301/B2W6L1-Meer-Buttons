8db4c000d6fe4ee2d98bbb9952db6d5aeee4d82e
Jason R <99057964@mydavinci.nl>
2021-01-06 10:25:01 +0100

    Opdracht B af.

--- opdracht B /buttons.js
+++ opdracht B /buttons.js
@@ -1,10 +1,17 @@
 //Lab --> Opdracht B --> 04-01-2021.
-var container = document.getElementById('container');
-i = 0;
-for(i=1; i < 31; i++){
-    var create = document.createElement('button');
-    create.innerHTML = i;
-    container.appendChild(create);
+var container = document.getElementById('container');//Variabele with the div element id.
+container.style.backgroundColor = "white";
+document.body.style.backgroundColor = "gray";
+
+for(i=0; i < 30; i++){
+    var button = document.createElement('button');
+    var text = document.createTextNode((i+1));
+        button.style.backgroundColor = "green";
+        button.appendChild(text);
+        button.onclick = function(){
+            this.style.backgroundColor = "red";
+        }  
+
+    container.appendChild(button);//Appereance the buttons in the div element.
 }
 
-console.dir(create);
